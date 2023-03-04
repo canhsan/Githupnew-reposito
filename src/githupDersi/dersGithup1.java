8cd51d7a2f9d3cb9f0f2c2973248a957e49ad167
canhsan <1979hasancan@gmail.com>
2023-03-05 00:47:06 +0300

    ilk değişklik

--- src/githupDersi/dersGithup1.java
+++ src/githupDersi/dersGithup1.java
@@ -3,6 +3,7 @@ package githupDersi;
 public class dersGithup1 {
     public static void main(String[] args) {
         System.out.println("Githup öğreniyorum");
+        System.out.println("güncellleme");
 
     }
 }
